9405c4e499f011c54ee67258185a5de67e5592b0
DanielSemu <danielsemu06@gmail.com>
2024-01-24 16:47:52 +0300

    sds

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,10 +1,23 @@
 
 import React from 'react'
-
+import './Mynav.css'
 const MyNav = () => {
   return (
     <div>
-      
+      <nav className='nav-menu'>
+        <div className='logo'>
+            <h1 className='font-signature'>Daniel</h1>
+        </div>
+        <div className='lists'>
+            <ul className='unordered'>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+                <li>Home</li>
+            </ul>
+
+        </div>
+      </nav>
     </div>
   )
 }
